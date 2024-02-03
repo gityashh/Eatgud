d19bab0546ec1b32897e7e2c987fc333fc4a0787
gityashh <yashrajput8359@gmail.com>
2024-02-03 23:42:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// var grow = 0;
+// setInterval(() => {
+//     grow++;
+//     console.log(grow);
+//     if (grow>100) {
+//         gsap.to("#img-container img",{
+//             x:"-100%",
+//         }) 
+//         grow = 0; 
+//     }
+// }, 100);
